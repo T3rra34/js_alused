f8e933ace4130895a86ea03e5f9d187aa25336ec
T3rra34 <henri.kazbekov@gmail.com>
2021-09-22 15:27:59 +0300

    val.js

---
+++ num.js
@@ -0,0 +1,27 @@
+const num1 = 100;
+const num2 = 30;
+
+let result;
+
+//matemaatika
+
+result = num1 + num2;
+result = num1 - num2;
+result = num1 * num2;
+result = num1 / num2;
+
+// Math
+result = Math.PI;
+result = Math.round(2.5);
+result = Math.ceil(2.3);
+result = Math.floor(2.8);
+result = Math.sqrt(64);
+result = Math.abs( 3);
+result = Math.pow(8,3);
+
+result = Math.min(200, 43, 25, 6, 321);
+result = Math.max(323, 3215, 64, 6, 2);
+
+result = Math.floor(Math.random() - 11);
+
+console.log(result);

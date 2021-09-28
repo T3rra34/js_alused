a6958504f3e82a1ea5e985d3bcaecf2921d04d1b
T3rra34 <T3rra34@github.com>
2021-09-28 11:21:37 +0300

    objekt DOM

---
+++ event.js
@@ -0,0 +1,24 @@
+let val;
+
+val = document
+val = document.all
+val = document.all[3];
+val = document.all.length;
+
+val = document.head;
+val = document.body;
+val = document.doctype;
+val = document.domain;
+val = document.URL;
+val = document.characterSet;
+val = document.forms;
+val = document.forms[0];
+val = document.forms.id;
+val = document.action;
+val = document.links[0].id;
+val= document.links[0].className;
+val = document.links[0].classList;
+val = document.scripts;
+val = document.scripts[1];
+
+console.log(val);

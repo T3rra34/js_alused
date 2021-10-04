64c9d54d08127962a0799b1ff1f8e6a6f907922a
T3rra34 <T3rra34@github.com>
2021-10-04 11:35:04 +0300

    Add task done

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // elemendi looming
-const form = document.querySelector(`form`);
-const taskInput = document.querySelector(`#task`);
-const heading = document.querySelector(`h4`);
+const form = document.querySelector('form');
+const taskInput = document.querySelector('#task');
+const heading = document.querySelector('h4');
 
 taskInput.value = '';
 
@@ -16,11 +16,26 @@ taskInput.value = '';
 //taskInput.addEventListener('keypress', runEvent);
 //taskInput.addEventListener('blue', runEvent);
 
-taskInput.addEventListener('cut', runEvent);
-taskInput.addEventListener('paste', runEvent);
+//taskInput.addEventListener('cut', runEvent);
+//taskInput.addEventListener('paste', runEvent);
+form.addEventListener('submit', AddTask);
 
-function runEvent(e) {
-	console.log(`Event type: ${e.type}`)
-	heading.innerText = e.target.value;
-	//e.preventDefault();
+function AddTask(e) {
+	const li = document.createElement('li');
+	const list = document.querySelector('ul');
+
+	li.className = "collection-item"
+	li.appendChild(document.createTextNode(taskInput.value));
+
+	const link = document.createElement('a');
+	link.className = 'secondary-content';
+	link.appendChild(document.createTextNode("X"))
+	link.setAttribute('href', '#')
+
+	li.appendChild(link);
+	list.appendChild(li);
+
+	taskInput.value = "";
+
+	e.preventDefault();
 }
